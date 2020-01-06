aff93249edde3478ee9d9fd23b82fa7f3ba68867
Joel Guerrero <joelg1014@hotmail.com>
2020-01-06 15:32:46 -0500

    164. Efectos con Jquery

---
+++ Practicas/Sección 11 [PRACTICA] Creando una Pagina para un Restaurante/js/efectos.js
@@ -0,0 +1,58 @@
+$(document).ready(function() {
+
+    // Efecto Menu
+    $('.navegacion a').each(function(index, elemento) {
+        $(this).css({
+            'top': '-200px'
+        });
+
+        $(this).animate({
+            top: '0'
+        }, 2000 + (index * 500));
+    });
+
+
+    // Efecto Header
+    if ($(window).width() > 800) {
+        $('header .textos').css({
+            opacity: 0,
+            marginTop: 0
+        });
+
+        $('header .textos').animate({
+            opacity: 1,
+            marginTop: '-52px'
+        }, 1500);
+    }
+    //Scroll Elementos Menu
+    var acercaDe = $('#acerca-de').offset().top,
+        menu = $('#platillos').offset().top,
+        galeria = $('#galeria').offset().top,
+        ubicacion = $('#ubicacion').offset().top;
+    // Boton Acerca de
+    $('#btn-acerca-de').on('click', function(e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: acercaDe
+        }, 500);
+    });
+    $('#btn-menu').on('click', function(e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: menu
+        }, 500);
+    });
+    $('#btn-galeria').on('click', function(e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: galeria
+        }, 500);
+    });
+    $('#btn-uicacion').on('click', function(e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: ubicacion
+        }, 500);
+    });
+
+});
