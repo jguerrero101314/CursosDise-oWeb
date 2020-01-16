360e360fa49bc8771d633764a9086cd03977c0eb
Joel Guerrero <joelg1014@hotmail.com>
2020-01-16 12:34:34 -0500

    203. Fotos, Clientes y Precios

--- Practicas/Startup/src/app/components/main/main.component.ts
+++ Practicas/Startup/src/app/components/main/main.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
-export class MainComponent implements OnInit {
+export class MainComponent  {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  p_basico:string[] = ['1 Cuenta', '10 GB de espacio', 'Dominio Personal'];
+  p_standard:string[] = ['5 Cuenta', '50 GB de espacio', 'Dominio Personal'];
+  p_premium:string[] = ['Cuentas Ilimitadas', '500 GB de espacio', 'Dominios Ilimitados'];
+ 
+ 
 
 }
