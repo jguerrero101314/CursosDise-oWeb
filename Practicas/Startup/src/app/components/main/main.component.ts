5da45a8b78013458c89053cd2392df98c36c4eb3
jguerrero1003 <joelg1014@hotmail.com>
2020-01-18 14:36:40 -0500

    207. Estilos CSS de los Precios

--- Practicas/Startup/src/app/components/main/main.component.ts
+++ Practicas/Startup/src/app/components/main/main.component.ts
@@ -10,7 +10,4 @@ export class MainComponent  {
   p_basico:string[] = ['1 Cuenta', '10 GB de espacio', 'Dominio Personal'];
   p_standard:string[] = ['5 Cuenta', '50 GB de espacio', 'Dominio Personal'];
   p_premium:string[] = ['Cuentas Ilimitadas', '500 GB de espacio', 'Dominios Ilimitados'];
- 
- 
-
 }
